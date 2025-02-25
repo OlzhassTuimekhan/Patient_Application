82b02febb2954be75246c41dae69da033025d435
OlzhassTuimekhan <olzhasstuimekhan@mail.ru>
2025-02-25 10:05:16 +0500

    version 1.1

--- app/src/main/java/kz/olzhass/kolesa/MainActivity.kt
+++ app/src/main/java/kz/olzhass/kolesa/MainActivity.kt
@@ -1,38 +1,36 @@
 package kz.olzhass.kolesa
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import kz.olzhass.kolesa.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.welcome_activity)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            buttonLogin.setOnClickListener {
+                val intent = Intent(this@MainActivity, MainPage::class.java)
+                startActivity(intent)
+            }
+        }
 
         Log.d("Main", "Created")
 
-        enableEdgeToEdge()
-    }
-    fun onClickListener(view: View) {
-        setContentView(R.layout.activity_2)
-    }
-    fun onClickToLogin(view: View) {
-        setContentView(R.layout.login_page)
-    }
-    fun onClickToReturnWelcome(view: View) {
-        setContentView(R.layout.welcome_activity)
-    }
-    fun listener2 (view: View) {
-        setContentView(R.layout.activity_main)
     }
 
 
@@ -41,7 +39,7 @@ class MainActivity : AppCompatActivity() {
         super.onStart()
     }
 
-    override fun onPause() {
+     override fun onPause() {
         Log.d("Main", "Paused")
         super.onPause()
 
@@ -51,5 +49,4 @@ class MainActivity : AppCompatActivity() {
         Log.d("Main", "Resumed")
         super.onResume()
     }
-
 }
--- app/src/main/java/kz/olzhass/kolesa/hello.kt
+++
@@ -1,7 +0,0 @@
-package kz.olzhass.kolesa
-
-class hello {
-    fun main(args : Array<String>) {
-        println("Hello, World!")
-    }
-}
